837eba7991af4edddc1eaebce9eaa5bb1f0527af
jo-lyn <e0007247@u.nus.edu>
2018-10-22 17:16:01 +0800

    add main menu script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+    private bool isSelected;
 
-    // Use this for initialization
+    void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") != 0 && !isSelected)
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            isSelected = true;
+        }
+    }
     public void PlayGame()
     {
         SceneManager.LoadScene("main");
@@ -15,5 +26,10 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private void onDisable()
+    {
+        isSelected = false;
     }
 }
