7d017ded759888415c63815e6eccfc61a52d9309
jo-lyn <e0007247@u.nus.edu>
2018-10-31 16:38:56 +0800

    add button selection animations

---
+++ Assets/Scripts/ButtonSelected.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonSelected : MonoBehaviour, ISelectHandler
+{
+	public Animator anim;
+    public void OnSelect(BaseEventData eventData)
+    {
+        Debug.Log(this.gameObject.name + " was selected");
+		anim.Play("buttonSelected");
+    }
+
+	public void OnDeselect(BaseEventData eventData)
+    {
+        Debug.Log(this.gameObject.name + " was deselected");
+		anim.Play("buttonDeselected");
+    }
+}
