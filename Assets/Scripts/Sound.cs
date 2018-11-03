2aa88ae329f7e8f9a952197643d0d2b4bb66ec5f
jo-lyn <e0007247@u.nus.edu>
2018-11-03 23:06:23 +0800

    add bg music

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+	[Range(0.1f, 3f)]
+    public float pitch;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public static SoundController instance;
+    public Sound[] sounds;
+
+    // Use this for initialization
+    void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
